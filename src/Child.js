887e511f81a24c285b6d3f9b62556b77e1898a97
Salome <sallynanjala254@gmail.com>
2022-07-20 22:22:59 +0300

    Done.

--- src/Child.js
+++ src/Child.js
@@ -3,6 +3,12 @@ import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor }) {
   return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+  return <div onClick={handleClick} className="child" style={{ backgroundColor: color }} />;
 }
-
+}
 export default Child;
